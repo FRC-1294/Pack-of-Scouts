8c84bddd5ee0064daadc3c1227eec89c11634e4d
Martin Taillefer <mataille@microsoft.com>
2023-02-01 20:44:14 -0800

    Fix things

--- PackOfScouts/ScoutPage.xaml.cs
+++ PackOfScouts/ScoutPage.xaml.cs
@@ -2,19 +2,10 @@ namespace PackOfScouts;
 
 public partial class ScoutPage : ContentPage
 {
-    private string[] listOfTeams;
-    public ScoutPage()
-    {
-        InitializeComponent();
-        var layout = (VerticalStackLayout)FindByName("lamo");
-        //CHANGE TO YOUR USER PATH
-        listOfTeams = File.ReadAllLines("C:\\Users\\user\\PackOfScouts\\PackOfScouts\\bin\\Debug\\net7.0-windows10.0.19041.0\\win10-x64\\PackOfScoutsData.txt");
-        
-        foreach (string team in listOfTeams)
-        {
-            layout.Children.Add(new Button { Text = team });
-        }
-    }
+	public ScoutPage()
+	{
+		InitializeComponent();
+	}
 
     private async void OnOpenScoutFileClicked(object sender, EventArgs e)
     {
