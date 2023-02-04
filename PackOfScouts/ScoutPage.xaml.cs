ac1aaeef023e8c840ffef2df5013a47a03e4ec40
ConfettiRaindrop <camilafigueira@outlook.com>
2023-02-04 15:57:12 -0800

    Basic auto visual elements

--- PackOfScouts/ScoutPage.xaml.cs
+++ PackOfScouts/ScoutPage.xaml.cs
@@ -1,12 +1,47 @@
+using Microsoft.Maui.ApplicationModel.DataTransfer;
+
 namespace PackOfScouts;
 
 public partial class ScoutPage : ContentPage
 {
-	public ScoutPage()
-	{
-		InitializeComponent();
-	}
+    public ScoutPage()
+    {
+        InitializeComponent();
+        double screenWidth = (int)DeviceDisplay.MainDisplayInfo.Width / 5;
+        //currently code can only math screen width of computer, not screen size of app -- fix later
+        Finish.WidthRequest = screenWidth;
+        SaveFile.WidthRequest = screenWidth;
+        OpenFile.WidthRequest = screenWidth;
 
+
+    }
+
+    void OnStepperValueChangedCone(object sender, ValueChangedEventArgs e)
+    {
+        double value = e.NewValue;
+        if (value == 1)
+        {
+            _displayConeLabel.Text = string.Format("{0} cone scored", value);
+        }
+        else
+        {
+            _displayConeLabel.Text = string.Format("{0} cone scored", value);
+        }
+
+    }
+
+    void OnStepperValueChangedCube(object sender, ValueChangedEventArgs e)
+    {
+        double value = e.NewValue;
+        if (value == 1)
+        {
+            _displayCubeLabel.Text = string.Format("{0} cube scored", value);
+        }
+        else
+        {
+            _displayCubeLabel.Text = string.Format("{0} cubes scored", value);
+        }
+    }
     private async void OnOpenScoutFileClicked(object sender, EventArgs e)
     {
         var d = new Dictionary<DevicePlatform, IEnumerable<string>>
