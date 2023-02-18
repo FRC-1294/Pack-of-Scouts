f49a7de2d0c50fbdf29d7e9a4ba0fb1ba6d4c4b1
atian7064 <56783429+atian7064@users.noreply.github.com>
2023-02-18 13:21:13 -0800

    bruh

--- PackOfScouts/MatchData.cs
+++ PackOfScouts/MatchData.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PackOfScouts
-{
-    internal class MatchData
-    {
-        
-    }
+namespace PackOfScouts;
+
+public class MatchData { 
+    internal int matchNumber;
+    internal int robotNumber;
+    
 }
+
