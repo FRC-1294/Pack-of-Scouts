e738b8a455f381c64ebe7680df4632d558359837
Martin Taillefer <mataille@microsoft.com>
2023-02-25 11:22:12 -0800

    Add a schedule entry

---
+++ PackOfScouts/ScheduleEmtry.cs
@@ -0,0 +1,12 @@
+﻿namespace PackOfScouts;
+
+internal sealed class ScheduleEntry
+{
+    public int MatchNumber { get; set; }
+    public int RedRobot1 { get; set; }
+    public int RedRobot2 { get; set; }
+    public int RedRobot3 { get; set; }
+    public int BlueRobot1 { get; set; }
+    public int BlueRobot2 { get; set; }
+    public int BlueRobot3 { get; set; }
+}
