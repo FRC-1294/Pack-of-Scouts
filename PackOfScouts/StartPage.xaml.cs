919eeda5549dff49b63d442b8528d3cf35fd1b4f
Martin Taillefer <mataille@microsoft.com>
2023-03-12 11:54:03 -0700

    Final touches

--- PackOfScouts/StartPage.xaml.cs
+++ PackOfScouts/StartPage.xaml.cs
@@ -10,8 +10,19 @@ public partial class StartPage : ContentPage
 	private async void OnScoutClicked(object sender, EventArgs e)
 	{
         var a = new ApplicationState();
-        await Navigation.PushAsync(new ScheduleSelectionPage(a));
-	}
+
+        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "PackOfScouts_MatchSchedule.json");
+        if (!File.Exists(path))
+        {
+			var next = new ImportSchedulePage();
+			await Navigation.PushAsync(next);
+			Navigation.InsertPageBefore(new ScheduleSelectionPage(a), next);
+        }
+		else
+		{
+            await Navigation.PushAsync(new ScheduleSelectionPage(a));
+        }
+    }
 
     private async void OnScoutLeadClicked(object sender, EventArgs e)
 	{
