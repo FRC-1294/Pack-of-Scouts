a6ccd1f823ebb48df3acfaba9f7ea3500bd6a09c
Martin Taillefer <mataille@microsoft.com>
2023-03-12 07:24:21 -0700

    Improve layout of match schedule page

--- PackOfScouts/MatchSchedulePage.xaml.cs
+++ PackOfScouts/MatchSchedulePage.xaml.cs
@@ -10,8 +10,6 @@ public partial class MatchSchedulePage : ContentPage
 
         this.appState = applicationState;
 
-        AddHeader();
-
         foreach (var entry in appState.ScheduleEntries)
         {
             var g = new Grid
@@ -97,7 +95,6 @@ public partial class MatchSchedulePage : ContentPage
             {
                 MatchNum = matchNum,
                 RobotNum = robotNum,
-                
             },
         };
 
@@ -111,58 +108,7 @@ public partial class MatchSchedulePage : ContentPage
         var bi = (ButtonInfo)b.CommandParameter;
         await Navigation.PushAsync(new ScoutPage(bi.MatchNum, bi.RobotNum, this.appState));
     }
-
-    private void AddHeader()
-    {
-        var g = new Grid
-        {
-            RowDefinitions =
-            {
-                new RowDefinition
-                {
-                    Height = GridLength.Auto
-                }
-            },
-
-            ColumnDefinitions =
-            {
-                new ColumnDefinition
-                {
-                    Width = new GridLength(80, GridUnitType.Absolute)
-                },
-
-                new ColumnDefinition
-                {
-                    Width = new GridLength(80 * 6, GridUnitType.Absolute)
-                },
-
-            }
-        };
-
-        g.Add(new Label
-        {
-            Text = "Match #",
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center,
-            FontSize = 15,
-            TextColor = new Color(255, 255, 255)
-        });
-
-        g.Add(new Label
-        {
-            Text = "Robots to scout",
-            HorizontalOptions = LayoutOptions.Center,
-            VerticalOptions = LayoutOptions.Center,
-            FontSize = 15,
-            TextColor = new Color(255, 255, 255)
-        }, 1);
-
-        _tableView.Root[0].Add(new ViewCell
-        {
-            View = g,
-        });
-    }
-
+    
     private sealed class ButtonInfo
     {
         internal int MatchNum;
